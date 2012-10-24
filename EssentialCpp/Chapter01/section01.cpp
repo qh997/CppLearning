b6d4dd1da986bded36b779919628c7fa43227a59
Geng Shuang <gengs@neusoft.com>
2012-10-24 16:28:06 +0800

    《Essential C++ 中文版》1.1 如何撰写 C++ 程序

---
+++ EssentialCpp/Chapter01/section01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string user_first_name;
+    string user_last_name;
+    cout << "Please enter your first name and last name: ";
+    cin >> user_first_name >> user_last_name;
+    cout << '\n'
+         << "Hello, "
+         << user_first_name
+         << ' '
+         << user_last_name
+         << " ... and goodbye!\n";
+
+    return 0;
+}
