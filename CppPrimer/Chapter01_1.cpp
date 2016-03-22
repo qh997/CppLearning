d4e94755739610f5792f240e4adc391cda8e9a06
Geng Shuang <qh997@hotmail.com>
2016-03-22 10:46:44 +0800

    CP 01_1

---
+++ CppPrimer/Chapter01_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Sales_item.h"
+
+int main(void) {
+	Sales_item total, trans;
+
+	if (std::cin >> total) {
+		while (std::cin >> trans) {
+			if (total.same_isbn(trans)) {
+				total += trans;
+			}
+			else {
+				std::cout << total << std::endl;
+				total = trans;
+			}
+		}
+		std::cout << total << std::endl;
+	}
+	else {
+		std::cout << "No data?!" << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
